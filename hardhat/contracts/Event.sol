22e678b402118fa64dca9263ab2be025bfc60158
Badisse Bouabdallah <badisse.bouabdallah@proton.me>
2022-12-03 17:36:37 +0100

    feat: update sc

--- hardhat/contracts/Event.sol
+++ hardhat/contracts/Event.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "./Ticket.sol";
 
 // Event contract
 contract Event {
-  using Counters for Counters.Counter;
+    using Counters for Counters.Counter;
     Counters.Counter public _eventIds;
 
     // Struct for storing event information
@@ -13,43 +14,53 @@ contract Event {
         string name;
         string date;
         string location;
+        Ticket ticket;
     }
 
     // Mapping from event id to event struct
-    mapping (uint => EventInfo) public events;
-
-    // Mapping from event id to ticket contract address
-    mapping (uint => address) public eventToTicket;
+    mapping(uint => EventInfo) public events;
 
     // Event for when an event is created
     event EventCreated(
         uint id,
-        address ticket,
         string name,
         string date,
-        string location
+        string location,
+        Ticket ticket
     );
 
     // Event for when an event is updated
-    event EventUpdated(
-        uint id,
-        string name,
-        string date,
-        string location
-    );
+    event EventUpdated(uint id, string name, string date, string location);
 
     // Function to create a new event
-    function createEvent(address ticket, string memory name, string memory date, string memory location) external returns(uint) {
+    function createEvent(
+        string memory _name,
+        string memory _date,
+        string memory _location,
+        string memory _ticketName,
+        string memory _ticketSymbol,
+        uint _ticketPrice,
+        uint _maxTicketSupply,
+        string memory _ticketURI
+    ) external returns (uint) {
         uint id = _eventIds.current();
+        Ticket ticket = new Ticket(
+            _ticketName,
+            _ticketSymbol,
+            _ticketPrice,
+            _maxTicketSupply,
+            _ticketURI
+        );
+
         events[id] = EventInfo({
-            name: name,
-            date: date,
-            location: location
+          name: _name,
+          date: _date,
+          location: _location,
+          ticket: ticket
         });
-        eventToTicket[id] = ticket;
 
         // Emit event to indicate that the event has been created
-        emit EventCreated(id, ticket, name, date, location);
+        emit EventCreated(id, _name, _date, _location, ticket);
 
         _eventIds.increment();
 
@@ -57,11 +68,16 @@ contract Event {
     }
 
     // Function to update an existing event
-    function updateEvent(uint id, string memory name, string memory date, string memory location) external {
-      events[id].name = name;
-      events[id].date = date;
-      events[id].location = location;
+    function updateEvent(
+        uint id,
+        string memory name,
+        string memory date,
+        string memory location
+    ) external {
+        events[id].name = name;
+        events[id].date = date;
+        events[id].location = location;
 
-      emit EventUpdated(id, name, date, location);
+        emit EventUpdated(id, name, date, location);
     }
 }
--- hardhat/contracts/Ticket.sol
+++ hardhat/contracts/Ticket.sol
@@ -1,60 +1,50 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.17;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 // Ticket contract
-contract Ticket {
+contract Ticket is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter public _ticketIds;
 
-    // Struct for storing ticket information
-    struct TicketInfo {
-        address payable owner;
-        uint price;
-        bool isSold;
-    }
-
-    // Mapping from ticket id to ticket struct
-    mapping(uint => TicketInfo) public tickets;
+    uint price;
+    uint maxTicketsSupply;
+    string uri;
 
     // Event for when a ticket is created
-    event TicketCreated(uint id, address owner, uint price);
+    event TicketMinted(uint id, address owner);
 
     // Event for when a ticket is sold
     event TicketSold(uint id, address owner, uint price);
 
+    constructor (
+        string memory _name,
+        string memory _symbol,
+        uint _price,
+        uint _maxTicketsSupply,
+        string memory _uri
+    ) ERC721(_name, _symbol) {
+      price = _price;
+      maxTicketsSupply = _maxTicketsSupply;
+      uri = _uri;
+    }
+
     // Function to create a new ticket
-    function createTicket(uint price) external returns (uint) {
+    function mintTicket() external payable returns (uint) {
+        require(_ticketIds.current() < maxTicketsSupply);
+        require(msg.value == price, 'You must send the ticket price value');
+
         uint id = _ticketIds.current();
-        tickets[id] = TicketInfo({
-            owner: payable(msg.sender),
-            price: price,
-            isSold: false
-        });
+        _mint(msg.sender, id);
 
         // Emit event to indicate that the ticket has been created
-        emit TicketCreated(id, msg.sender, price);
+        emit TicketMinted(id, msg.sender);
 
         _ticketIds.increment();
 
         return id;
     }
 
-    // Function to purchase a ticket
-    function purchaseTicket(uint id) external {
-        // Check if ticket is valid and not already sold
-        require(id < _ticketIds.current(), "Ticket does not exist");
-        require(!tickets[id].isSold, "Ticket has already been sold");
-
-        // Transfer ticket ownership and mark as sold
-        tickets[id].owner = payable(msg.sender);
-        tickets[id].isSold = true;
-
-        // Transfer funds from buyer to seller
-        tickets[id].owner.transfer(tickets[id].price);
-
-        // Emit event to indicate that the ticket has been sold
-        emit TicketSold(id, msg.sender, tickets[id].price);
-    }
 }
