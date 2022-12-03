20e4964c2e0671dced06897954f878f4e653bc6a
Badisse Bouabdallah <badisse.bouabdallah@proton.me>
2022-12-03 20:23:20 +0100

    feat: add navbar

--- marketplace/src/pages/_app.tsx
+++ marketplace/src/pages/_app.tsx
@@ -3,9 +3,27 @@ import { type AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import type { Menu } from "../components/Navbar";
+import Navbar from "../components/Navbar";
+
+const menus: Menu[] = [
+  {
+    href: '/',
+    title: 'Home'
+  },
+  {
+    href: '/about',
+    title: 'About'
+  },
+]
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar menus={menus} />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
