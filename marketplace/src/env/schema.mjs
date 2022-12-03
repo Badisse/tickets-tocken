629e65934344893960f3ef779a696edc9aaf8f93
Badisse Bouabdallah <badisse.bouabdallah@proton.me>
2022-12-03 14:53:29 +0100

    feat: add navbar

--- marketplace/src/env/schema.mjs
+++ marketplace/src/env/schema.mjs
@@ -17,6 +17,8 @@ export const serverSchema = z.object({
  */
 export const clientSchema = z.object({
   // NEXT_PUBLIC_BAR: z.string(),
+  NEXT_PUBLIC_NFT_ADDRESS: z.string(),
+  NEXT_PUBLIC_NFT_MARKETPLACE_ADDRESS: z.string()
 });
 
 /**
@@ -27,4 +29,6 @@ export const clientSchema = z.object({
  */
 export const clientEnv = {
   // NEXT_PUBLIC_BAR: process.env.NEXT_PUBLIC_BAR,
+  NEXT_PUBLIC_NFT_ADDRESS: process.env.NEX_PUBLIC_NFT_ADDRESS,
+  NEXT_PUBLIC_NFT_MARKETPLACE_ADDRESS: process.env.NEX_PUBLIC_NFT_ADDRESS
 };
--- marketplace/src/pages/_app.tsx
+++ marketplace/src/pages/_app.tsx
@@ -3,9 +3,31 @@ import { type AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import Link from "next/link";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">Tickets Tockens</p>
+        <div className="flex mt-4">
+          <Link href="/" className="mr-6 text-pink-500">
+              Home
+          </Link>
+          <Link href="/create-ticket" className="mr-6 text-pink-500">
+              Sell Ticket
+          </Link>
+          <Link href="/my-tickets" className="mr-6 text-pink-500">
+              My Tickets
+          </Link>
+          <Link href="/creator-dashboard" className="mr-6 text-pink-500">
+            Creator Dashboard
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
