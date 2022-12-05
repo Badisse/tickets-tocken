d9497475303c94ea07decbc3e9e7513edc596ff9
Badisse Bouabdallah <badisse.bouabdallah@proton.me>
2022-12-05 15:42:10 +0100

    feat: update design

--- marketplace/src/components/Navbar.tsx
+++ marketplace/src/components/Navbar.tsx
@@ -12,60 +12,65 @@ type Props = {
   menus: Menu[]
 }
 
-const Navbar = ({menus}: Props) => {
+const Navbar = ({ menus }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const {
     state: { account },
     dispatch,
   } = useEth();
-  
+
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-blue-500 p-6">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <span className="font-semibold text-xl tracking-tight">Tickets Tocken</span>
-      </div>
-      <div className="block lg:hidden">
-        <button
-          className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
-        </button>
-      </div>
-      <div className={`${isOpen ? "block" : "hidden"} w-full block flex-grow lg:flex lg:items-center lg:w-auto`}>
-        <div className="text-sm lg:flex-grow">
-          {
-            menus.map((menu: Menu, index: number) => (
-              <Link
-                key={index}
-                href={menu.href}
-                className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-              >
-                {menu.title}
-              </Link>
-            ))
-          }
+    <nav className="relative bg-white shadow-lg">
+      <div className="flex items-center justify-between px-6 py-3 bg-gray-200">
+        <div className="flex items-center">
+          <span className="font-semibold text-xl tracking-tight text-gray-700">Tickets Tocken</span>
         </div>
+        <div className="block lg:hidden">
+          <button
+            className="px-3 py-2 rounded-lg focus:outline-none focus:text-gray-500"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div className={`${isOpen ? "block" : "hidden"} px-2 pt-2 pb-4 mt-2 bg-gray-200`}>
+        {
+          menus.map((menu: Menu, index: number) => (
+            <Link
+              key={index}
+              href={menu.href}
+              className="block mt-1 mx-2 text-sm font-semibold rounded-lg focus:outline-none focus:bg-gray-300"
+            >
+              {menu.title}
+            </Link>
+          ))
+        }
+      </div>
+      <div className="px-6 py-3 bg-gray-200">
         {
           account
             ? (
-              <div>
+              <div className="text-sm text-gray-700">
                 {account}
               </div>
             )
             : (
               <button
-                className="rounded-lg bg-white p-2"
+                className="px-4 py-2 font-bold text-gray-800 rounded-lg bg-white focus:outline-none focus:shadow-outline"
                 onClick={() => connectWallet(dispatch)}
               >
                 Connect Wallet
               </button>
             )
         }
-        
+
       </div>
     </nav>
-)}
+  )
+}
 
 export default Navbar;
